59f0e321e635c03d4dc634446c629087f6419b6d
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-05-14 07:00:15 +0400

    test

--- src/index.js
+++ src/index.js
@@ -17,19 +17,6 @@ root.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/service-worker.js").then(
-            (registration) => {
-                console.log("Service Worker registration successful with scope: ", registration.scope);
-            },
-            (err) => {
-                console.log("Service Worker registration failed: ", err);
-            }
-        );
-    });
-}
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
